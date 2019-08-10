5ff3e1db9882cbb2f0c2e920fd4dcdae75838449
ohtorii <ikeuchi.h9@gmail.com>
2019-08-10 21:05:09 +0900

    First commit.

---
+++ hm_process/dll_interface.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace hm_process
+{
+    public class dll_interface
+    {
+        [DllExport]
+        public static int Spawn(IntPtr filename, IntPtr arguments)
+        {
+			return process_holder.Spawn(Marshal.PtrToStringAuto(filename), Marshal.PtrToStringAuto(arguments));
+		}
+
+		[DllExport]
+		public static IntPtr Start(IntPtr handle)
+		{
+			if (process_holder.Start(handle.ToInt32()))
+			{
+				return new IntPtr(1);	//true
+			}
+			return new IntPtr(0);		//false
+		}
+
+		[DllExport]
+		public static IntPtr WaitForExit(IntPtr handle)
+		{
+			if (process_holder.WaitForExit(handle.ToInt32()))
+			{
+				return new IntPtr(1);   //true
+			}
+			return new IntPtr(0);   //false
+		}
+
+		[DllExport]
+		public static IntPtr WaitForExitWithTimeOut(IntPtr handle,IntPtr timeout_msec)
+		{
+			if (process_holder.WaitForExit(handle.ToInt32(),timeout_msec.ToInt32()))
+			{
+				return new IntPtr(1);   //true
+			}
+			return new IntPtr(0);   //false
+		}
+
+		[DllExport]
+		public static IntPtr HasExited(IntPtr handle)
+		{
+			if (process_holder.HasExited(handle.ToInt32()))
+			{
+				return new IntPtr(1);   //true
+			}
+			return new IntPtr(0);   //false
+		}
+
+		[DllExport]
+		public static IntPtr ExitCode(IntPtr handle)
+		{
+			return new IntPtr(process_holder.ExitCode(handle.ToInt32()));
+		}
+	}
+}
---
+++ hm_process/process_holder.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+
+namespace hm_process
+{
+	class process_holder
+	{
+		class process
+		{
+			public ProcessStartInfo _start_info;
+			public Process _process;
+		}
+
+		public static int Spawn(string filename, string arguments)
+		{
+			var item = new process();
+			item._start_info = new ProcessStartInfo(filename, arguments);
+			item._start_info.UseShellExecute = true;
+
+			var current_handle = _next_handle;
+			_process[current_handle] = item;
+			++_next_handle;
+			return current_handle;
+		}
+
+		public static bool Start(int handle)
+		{
+			try
+			{
+				_process[handle]._process = Process.Start(_process[handle]._start_info);
+				return true;
+			}
+			catch (Exception)
+			{
+				//pass
+			}
+			return false;
+		}
+
+		public static bool WaitForExit(int handle)
+		{
+			try
+			{
+				_process[handle]._process.WaitForExit();
+				return true;
+			}
+			catch (Exception)
+			{
+				//pass
+			}
+			return false;
+		}
+
+		public static bool WaitForExit(int handle, int timeout)
+		{
+			try
+			{
+				return _process[handle]._process.WaitForExit(timeout);
+			}
+			catch (Exception)
+			{
+				//pass
+			}
+			return false;
+		}
+
+		public static bool HasExited(int handle)
+		{
+			try
+			{
+				return _process[handle]._process.HasExited;
+			}
+			catch (Exception)
+			{
+				//pass
+			}
+			return false;
+		}
+
+		public static int ExitCode(int handle)
+		{
+			try
+			{
+				return _process[handle]._process.ExitCode;
+			}
+			catch (Exception)
+			{
+				//pass
+			}
+			return 0;
+		}
+
+
+		static int _next_handle = 1;
+		static Dictionary<int, process> _process = new Dictionary<int, process>();
+
+	}
+}
