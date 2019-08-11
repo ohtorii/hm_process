4ba07204f4924871667740dc58c7fb308057f9fe
ohtorii <ikeuchi.h9@gmail.com>
2019-08-11 14:35:19 +0900

    Update dll_interface.cs

--- hm_process/dll_interface.cs
+++ hm_process/dll_interface.cs
@@ -13,13 +13,23 @@ namespace hm_process
         [DllExport]
         public static int Spawn(IntPtr filename, IntPtr arguments)
         {
-			return process_holder.Spawn(Marshal.PtrToStringAuto(filename), Marshal.PtrToStringAuto(arguments));
+			return ProcessHolder.Spawn(Marshal.PtrToStringAuto(filename), Marshal.PtrToStringAuto(arguments));
+		}
+
+		[DllExport]
+		public static int SpawnWithRedirect(IntPtr filename, IntPtr arguments, IntPtr redirect_stndard_output, IntPtr redirect_standard_error)
+		{
+			return ProcessHolder.SpawnWithRedirect(
+					Marshal.PtrToStringAuto(filename), 
+					Marshal.PtrToStringAuto(arguments), 
+					redirect_stndard_output.ToInt32()==0?false:true,
+					redirect_standard_error.ToInt32()==0?false:true);
 		}
 
 		[DllExport]
 		public static IntPtr Start(IntPtr handle)
 		{
-			if (process_holder.Start(handle.ToInt32()))
+			if (ProcessHolder.Start(handle.ToInt32()))
 			{
 				return new IntPtr(1);	//true
 			}
@@ -27,9 +37,21 @@ namespace hm_process
 		}
 
 		[DllExport]
+		public static IntPtr ReadStandardOutput(IntPtr handle)
+		{
+			return ProcessHolder.ReadStandardOutput(handle.ToInt32());
+		}
+
+		[DllExport]
+		public static IntPtr ReadStandardError(IntPtr handle)
+		{
+			return ProcessHolder.ReadStandardError(handle.ToInt32());
+		}
+
+		[DllExport]
 		public static IntPtr WaitForExit(IntPtr handle)
 		{
-			if (process_holder.WaitForExit(handle.ToInt32()))
+			if (ProcessHolder.WaitForExit(handle.ToInt32()))
 			{
 				return new IntPtr(1);   //true
 			}
@@ -39,7 +61,7 @@ namespace hm_process
 		[DllExport]
 		public static IntPtr WaitForExitWithTimeOut(IntPtr handle,IntPtr timeout_msec)
 		{
-			if (process_holder.WaitForExit(handle.ToInt32(),timeout_msec.ToInt32()))
+			if (ProcessHolder.WaitForExit(handle.ToInt32(),timeout_msec.ToInt32()))
 			{
 				return new IntPtr(1);   //true
 			}
@@ -49,17 +71,28 @@ namespace hm_process
 		[DllExport]
 		public static IntPtr HasExited(IntPtr handle)
 		{
-			if (process_holder.HasExited(handle.ToInt32()))
+			if (ProcessHolder.HasExited(handle.ToInt32()))
+			{
+				return new IntPtr(1);   //true
+			}
+			return new IntPtr(0);   //false
+		}
+		[DllExport]
+		public static IntPtr ExitCode(IntPtr handle)
+		{
+			return new IntPtr(ProcessHolder.ExitCode(handle.ToInt32()));
+		}
+
+		[DllExport]
+		public static IntPtr Destroy(IntPtr handle)
+		{
+			if (ProcessHolder.Destroy(handle.ToInt32()))
 			{
 				return new IntPtr(1);   //true
 			}
 			return new IntPtr(0);   //false
 		}
 
-		[DllExport]
-		public static IntPtr ExitCode(IntPtr handle)
-		{
-			return new IntPtr(process_holder.ExitCode(handle.ToInt32()));
-		}
+
 	}
 }
